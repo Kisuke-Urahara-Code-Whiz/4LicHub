37cd757309421f490338843fc1231c71bf767cb1
Sougata Mondal <sougatacodewhiz@gmail.com>
2026-06-13 10:46:14 +0530

    initialised the directories of chatService and playerService

---
+++ Backend/chatService/src/test/java/_LicHub/Backend/chatService/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package _LicHub.Backend.chatService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
